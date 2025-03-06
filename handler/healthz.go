03331625502fd96c86182c89019a839716e75a48
BIsetsuko <seina.nagato@blueish.co.jp>
2025-03-07 01:32:21 +0900

    jsonにエンコードしてエラーをlogで吐かせる

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +18,18 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	res := &model.HealthzResponse{Message: "OK"}// レスポンス用の構造体を作成
+
+	w.Header().Set("Content-Type", "application/json") // レスポンスヘッダーにJSONであることを設定
+	if err := json.NewEncoder(w).Encode(res);err != nil {
+		log.Println(err)
+	}
+// ↑の構文がよくわからなかったけど、以下をまとめて書くとこうなるらしい
+// 	// 1つ目の処理：JSONエンコードを実行して結果をerrに格納
+// err := json.NewEncoder(w).Encode(res)
+
+// // 2つ目の処理：errがnilでないかチェック
+// if err != nil {
+//     log.Println(err)
+// }
 }
