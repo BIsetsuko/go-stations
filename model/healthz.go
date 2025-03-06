40a8ce2b470515298b4eedbce74913bb1f8c9d07
BIsetsuko <seina.nagato@blueish.co.jp>
2025-03-06 23:56:00 +0900

    HealthzResponseを定義

--- model/healthz.go
+++ model/healthz.go
@@ -1,4 +1,7 @@
 package model
 
 // A HealthzResponse expresses health check message.
-type HealthzResponse struct{}
+type HealthzResponse struct{
+	//   string 型で Message という名前の Field を定義
+	Message string
+}
